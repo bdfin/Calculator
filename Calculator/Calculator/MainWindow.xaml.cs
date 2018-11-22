89775b1f7d7821b9df476be1bbf03b20f0828c1d
bdfin <beau.d.findlay@gmail.com>
2018-11-22 17:34:54 +0000

    Update number button click events

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace Calculator
 
         decimal num1 = 0;
         decimal num2 = 0;
+        string operation = "";
 
         public MainWindow()
         {
@@ -31,62 +32,143 @@ namespace Calculator
 
         private void Btn1_Click(object sender, RoutedEventArgs e)
         {
-            num1 = (num1 * 10) + 1;
-            txtDisplay.Text = num1.ToString();
+            if (operation == "")
+            {
+                num1 = (num1 * 10) + 1;
+                txtDisplay.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = (num1 * 10) + 1;
+                txtDisplay.Text = num2.ToString();
+            }
+
         }
 
         private void Btn2_Click(object sender, RoutedEventArgs e)
         {
-            num1 = (num1 * 10) + 2;
-            txtDisplay.Text = num1.ToString();
+            if (operation == "")
+            {
+                num1 = (num1 * 10) + 2;
+                txtDisplay.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = (num1 * 10) + 2;
+                txtDisplay.Text = num2.ToString();
+            }
         }
 
         private void Btn3_Click(object sender, RoutedEventArgs e)
         {
-            num1 = (num1 * 10) + 3;
-            txtDisplay.Text = num1.ToString();
+            if (operation == "")
+            {
+                num1 = (num1 * 10) + 3;
+                txtDisplay.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = (num1 * 10) + 3;
+                txtDisplay.Text = num2.ToString();
+            }
         }
 
         private void Btn4_Click(object sender, RoutedEventArgs e)
         {
-            num1 = (num1 * 10) + 4;
-            txtDisplay.Text = num1.ToString();
+            if (operation == "")
+            {
+                num1 = (num1 * 10) + 4;
+                txtDisplay.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = (num1 * 10) + 4;
+                txtDisplay.Text = num2.ToString();
+            }
         }
 
         private void Btn5_Click(object sender, RoutedEventArgs e)
         {
-            num1 = (num1 * 10) + 5;
-            txtDisplay.Text = num1.ToString();
+            if (operation == "")
+            {
+                num1 = (num1 * 10) + 5;
+                txtDisplay.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = (num1 * 10) + 5;
+                txtDisplay.Text = num2.ToString();
+            }
         }
 
         private void Btn6_Click(object sender, RoutedEventArgs e)
         {
-            num1 = (num1 * 10) + 6;
-            txtDisplay.Text = num1.ToString();
+            if (operation == "")
+            {
+                num1 = (num1 * 10) + 6;
+                txtDisplay.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = (num1 * 10) + 6;
+                txtDisplay.Text = num2.ToString();
+            }
         }
 
         private void Btn7_Click(object sender, RoutedEventArgs e)
         {
-            num1 = (num1 * 10) + 7;
-            txtDisplay.Text = num1.ToString();
+            if (operation == "")
+            {
+                num1 = (num1 * 10) + 7;
+                txtDisplay.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = (num1 * 10) + 7;
+                txtDisplay.Text = num2.ToString();
+            }
         }
 
         private void Btn8_Click(object sender, RoutedEventArgs e)
         {
-            num1 = (num1 * 10) + 8;
-            txtDisplay.Text = num1.ToString();
+            if (operation == "")
+            {
+                num1 = (num1 * 10) + 8;
+                txtDisplay.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = (num1 * 10) + 8;
+                txtDisplay.Text = num2.ToString();
+            }
         }
 
         private void Btn9_Click(object sender, RoutedEventArgs e)
         {
-            num1 = (num1 * 10) + 9;
-            txtDisplay.Text = num1.ToString();
+            if (operation == "")
+            {
+                num1 = (num1 * 10) + 9;
+                txtDisplay.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = (num1 * 10) + 9;
+                txtDisplay.Text = num2.ToString();
+            }
         }
 
         private void Btn0_Click(object sender, RoutedEventArgs e)
         {
-            num1 = (num1 * 10) + 0;
-            txtDisplay.Text = num1.ToString();
+            if (operation == "")
+            {
+                num1 = (num1 * 10);
+                txtDisplay.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = (num1 * 10);
+                txtDisplay.Text = num2.ToString();
+            }
         }
     }
 }
